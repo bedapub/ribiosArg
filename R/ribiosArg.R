82df53ac4853f977274d35cdf0f71fb03089dd17
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-04-06 11:23:41 +0200

    document the ribiosArg package

---
+++ R/ribiosArg.R
@@ -0,0 +1,7 @@
+#' Command-line argument handling in for R scripting
+#' @docType package
+#' @author Jitao David Zhang <jitao_david.zhang@roche.com>
+#' @useDynLib ribiosArg, .registration=TRUE, .fixes="C_"
+#' @name ribiosArg-package
+#' @depends ribiosUtils
+NULL
