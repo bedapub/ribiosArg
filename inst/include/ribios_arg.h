ea04b786869641b6e3a30222d4745a2c32e0b258
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-08-14 14:28:50 +0200

    funcs from ribiosUtils are deleted

--- inst/include/ribios_arg.h
+++ inst/include/ribios_arg.h
@@ -11,15 +11,6 @@ extern "C" {
 
 #define CALLMETHOD_DEF(fun, numArgs) {#fun, (DL_FUNC) &fun, numArgs}
 
-// functions from ribiosUtils
-void(*usage)(char*, ...);
-void(*strReplace)(char**, char*);
-int(*arg_init)(int, char*[], char*, char*, void (*usagef)(int));
-int(*arg_isInit)(void);
-char*(*arg_getPos)(char*, int);
-int(*arg_present)(char*);
-void*(*hlr_callocs)(size_t, size_t);
-
 extern SEXP rarg_isInit();
 extern SEXP rarg_parse(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP rarg_get(SEXP);
