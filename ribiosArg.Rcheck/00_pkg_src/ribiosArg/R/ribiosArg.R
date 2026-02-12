af8b851715ce86948c3024f2d5bf5830e404009d
Jitao David Zhang <davidvonpku@gmail.com>
2026-02-12 11:48:05 +0100

    remove ribiosArg.Rcheck files

--- ribiosArg.Rcheck/00_pkg_src/ribiosArg/R/ribiosArg.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-#' ribiosIO
-#' ribiosIO provides Command-line argument handling for R scripting
-#' @author Jitao David Zhang <jitao_david.zhang@roche.com>
-#' @useDynLib ribiosArg, .registration=TRUE, .fixes="C_"
-#' @name ribiosArg
-NULL
--- ribiosArg.Rcheck/tests/test_scriptName.R
+++
@@ -1,8 +0,0 @@
-#!/bin/tcsh /SOFT/bi/apps/R/bdeRscript
-
-## test getArg
-library(ribiosArg)
-print(scriptName())
-print(commandArgs())
-stopifnot(identical("test_scriptName.R", scriptName()))
-
